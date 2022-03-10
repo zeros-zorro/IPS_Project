35455d4925e4f4d064fbd48ebb24bbc485fabbbc
jchaverot <jeremy.chaverot@epfl.ch>
2022-03-10 10:54:09 +0100

    added the menu

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Vector3 m_Movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,8 +18,8 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        m_Movement.Set(horizontal, 0f, vertical);
-        m_Movement.Normalize();
-        transform.Translate(Time.deltaTime * m_Movement);
+        m_Movement.Set(horizontal, 0f, vertical) ;
+        m_Movement.Normalize() ;
+        transform.Translate(Time.deltaTime* m_Movement) ;
     }
 }
