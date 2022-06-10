0f1e240f4994605291241712ff283ad158808a35
jchaverot <jeremy.chaverot@epfl.ch>
2022-06-10 16:19:10 +0200

    new version

--- Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
+++ Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
@@ -17,10 +17,12 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
     public static string HORIZONTAL_WASD = "HorizontalWASD";
     public static string VERTICAL_WASD = "VerticalWASD";
 
+    Steering steering = new Steering();
     private GameManager game;
     private InputKeyboard inputKeyboard;
     private string verticalAxis = null;
     private string horizontalAxis = null;
+    bool collisionBehavior = false;
 
     public void Start()
     {
@@ -36,9 +38,8 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
 
     public override Steering GetSteering()
     {
-        Steering steering = new Steering();
 
-        if (game.GetGameRunningStatus())
+        if (game.GetGameRunningStatus() && !collisionBehavior)
         {
             float horizontal = Input.GetAxis(horizontalAxis);
             float vertical = Input.GetAxis(verticalAxis);
@@ -46,6 +47,9 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
 
             steering.linear = new Vector3(horizontal, 0, vertical) * agent.maxAccel;
             steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.linear, agent.maxAccel));
+
+        } else if (!game.GetGameRunningStatus()) { 
+            steering = new Steering();
         }
      
         return steering;
@@ -58,4 +62,19 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
             game.StartGame();
         }
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        agent.ActivateDirectionalHapticFeedback();
+        foreach (ContactPoint contact in collision.contacts)
+            steering.linear = collision.contacts[0].normal.normalized * agent.maxAccel;
+        collisionBehavior = true;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        agent.DeActivateDirectionalHapticFeedback();
+        collisionBehavior = false;
+    }
+
 }
