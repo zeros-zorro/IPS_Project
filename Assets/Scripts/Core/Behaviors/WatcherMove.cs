cfd1fbc1c504c6f4d00b11be3912bc715ca91cc0
SebastianMaierEPFL <100838445+SebastianMaierEPFL@users.noreply.github.com>
2022-06-10 17:58:57 +0200

    path

--- Assets/Scripts/Core/Behaviors/WatcherMove.cs
+++ Assets/Scripts/Core/Behaviors/WatcherMove.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class WatcherMove : AgentBehaviour
 {
     public Guard guard;
-
     enum WatcherState
     {
         IDLE,
@@ -17,6 +16,7 @@ public class WatcherMove : AgentBehaviour
     public float angularThreshold = 10.0f;
     public float rotationSpeed = 10.0f;
     public Transform target;
+    [SerializeField]
     private WatcherState watcherState;
 
     private void Start()
@@ -24,50 +24,35 @@ public class WatcherMove : AgentBehaviour
         watcherState = WatcherState.SEARCH;
     }
 
+    private void Update()
+    {
+        if(watcherState == WatcherState.SEARCH)
+        {
+            target = guard.targetWaypoint;
+        }
+    }
     public override Steering GetSteering()
     {
         Steering steering = new Steering();
-
+        
         Vector2 position = new Vector2(this.transform.position.x, this.transform.position.z);
         Vector2 forward = new Vector2(this.transform.forward.x, this.transform.forward.z);
         Vector2 targetPosition = new Vector2(target.position.x, target.position.z);
-
         float angleToTarget = -Vector2.SignedAngle(forward, targetPosition - position);
         float angularFactor = angleToTarget / 180f;
-        if(watcherState == WatcherState.PURSUE)
+        steering.angular = angularFactor * rotationSpeed;
+        if (Mathf.Abs(angleToTarget) < angularThreshold)
         {
-
-            steering.angular = angularFactor * rotationSpeed;
-
-            if (Mathf.Abs(angleToTarget) < angularThreshold)
+            
+            if (agent.GetVelocity() == Vector3.zero)
             {
-                if (agent.GetVelocity() == Vector3.zero)
-                {
-                    steering.angular = 0;
-                }
-
-                steering.linear = Vector3.forward * speed * agent.maxAccel;
-                steering.linear = this.transform.TransformDirection(Vector3.ClampMagnitude(steering.linear, agent.maxAccel));
-
+                steering.angular = 0f;
             }
+            
+            steering.linear = Vector3.forward * speed * agent.maxAccel;
+            steering.linear = this.transform.TransformDirection(Vector3.ClampMagnitude(steering.linear, agent.maxAccel));
         }
-        if(watcherState == WatcherState.SEARCH)
-        {
-            Vector3 direction = guard.targetWaypoint - transform.position;
-            if (Vector3.Distance(transform.position, guard.targetWaypoint) < 0.1f)
-            {
-                steering.linear = Vector3.zero;
-                steering.angular = angularFactor * rotationSpeed;
-            }
-            else
-            {
-                steering.angular = 0;
-                steering.linear = direction * agent.maxAccel;
-                steering.linear = this.transform.TransformDirection(Vector3.ClampMagnitude(steering.linear, agent.maxSpeed));
-            }
-        }
-
-
+        
         return steering;
     }
 }
--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -1,97 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Guard : AgentBehaviour
 {
-
     public float speed = 5f;
     public float waitTime = 3f;
-
     public Transform pathHolder;
-
-    public Vector3 targetWaypoint;
-    private Vector3 nextTargetWaypoint;
-    private Rigidbody rb;
-
+    public Transform targetWaypoint;
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        Vector3[] waypoints = new Vector3[pathHolder.childCount];
-        for(int i = 0; i < waypoints.Length; i++)
+        //Vector3[] waypoints = new Vector3[pathHolder.childCount];
+        Transform[] waypoints = new Transform[pathHolder.childCount];
+        for (int i = 0; i < waypoints.Length; i++)
         {
-            waypoints[i] = pathHolder.GetChild(i).position;
-            waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z);
+            //waypoints[i] = pathHolder.GetChild(i).position;
+            waypoints[i] = pathHolder.GetChild(i);
+            //waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z);
+
         }
-
         StartCoroutine(FollowPath(waypoints));
-        
+
     }
-
-    IEnumerator FollowPath(Vector3[] waypoints)
+    IEnumerator FollowPath(Transform[] waypoints)
     {
-        transform.position = waypoints[0];
-        //transform.forward = 
+        //transform.position = waypoints[0];
+        transform.position = waypoints[0].position;
         int targetWaypointIndex = 1;
+        //this.targetWaypoint = waypoints[targetWaypointIndex];
         this.targetWaypoint = waypoints[targetWaypointIndex];
-        this.targetWaypoint = waypoints[targetWaypointIndex + 1];
-
-        yield return new WaitForSeconds(waitTime);
         while (true)
         {
-            //transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
-            if(isSpeedNull() && isFacingTarget())
+            if (Vector3.Distance(transform.position, targetWaypoint.position) < 0.05f)
             {
+                Debug.Log("Changed target");
                 targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
                 targetWaypoint = waypoints[targetWaypointIndex];
-                nextTargetWaypoint = waypoints[(targetWaypointIndex + 1) % waypoints.Length];
                 yield return new WaitForSeconds(waitTime);
             }
             yield return null;
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Vector3 startPosition = pathHolder.GetChild(0).position;
-        Vector3 previousPosition = startPosition;
-        foreach(Transform waypoint in pathHolder)
-        {
-            Gizmos.DrawSphere(waypoint.position, .3f);
-            Gizmos.DrawLine(previousPosition, waypoint.position);
-            previousPosition = waypoint.position;
-        }
-        Gizmos.DrawLine(previousPosition, startPosition);
-    }
-    /*
-    public override Steering GetSteering()
-    {
-        Steering steering = new Steering();
-
-        //TODO: if statement of if the game is running
-        Vector3 direction = targetWaypoint - transform.position;
-        if(Vector3.Distance(transform.position, targetWaypoint) < 0.05f)
-        {
-            steering.linear = Vector3.zero;
-        }
-        else
-        {
-            steering.linear = direction * agent.maxAccel;
-            steering.linear = this.transform.TransformDirection(Vector3.ClampMagnitude(steering.linear, agent.maxAccel));
-        }
-
-        return steering;
-    }
-    */
-
-    private bool isSpeedNull()
-    {
-        return (rb.velocity.magnitude < Vector3.kEpsilon);
-    }
-    private bool isFacingTarget()
-    {
-        float angleToTarget = -Vector2.SignedAngle(this.transform.forward, nextTargetWaypoint - this.transform.position);
-        float angularFactor = angleToTarget / 180f;
-        return (Mathf.Abs(angleToTarget) < 10.0f);
-    }
 }
+    }
+    private void OnDrawGizmos()
+{
+    Vector3 startPosition = pathHolder.GetChild(0).position;
+    Vector3 previousPosition = startPosition;
+    foreach (Transform waypoint in pathHolder)
+    {
+        Gizmos.DrawSphere(waypoint.position, .3f);
+        Gizmos.DrawLine(previousPosition, waypoint.position);
+        previousPosition = waypoint.position;
+    }
+    Gizmos.DrawLine(previousPosition, startPosition);
+}
+    /*
+public override Steering GetSteering()
+{
+    Steering steering = new Steering();
+    //TODO: if statement of if the game is running
+    Vector3 direction = targetWaypoint - transform.position;
+    if (Vector3.Distance(transform.position, targetWaypoint) < 0.1f)
+    {
+        steering.linear = Vector3.zero;
+    }
+    else
+    {
+        steering.linear = direction * agent.maxAccel;
+        steering.linear = this.transform.TransformDirection(Vector3.ClampMagnitude(steering.linear, agent.maxAccel));
+    }
+    return steering;
+}
+    */
+}
+
